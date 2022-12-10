9a1b5c9ff7a91ef79057945970d2dd65ea60a571
Ruslan Mukminov JD-CP-3 <yarus1987@gmail.com>
2022-12-10 13:45:25 +0300

    added display of maximum category for year, month, day and added Unit Tests

--- src/test/java/FinanceManagerTest.java
+++ src/test/java/FinanceManagerTest.java
@@ -1,36 +1,54 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 class FinanceManagerTest {
-    List<String> values = Arrays.asList("булка", "колбаса");
-    public Map<String, List<String>> categories = Map.of(
-            "еда", values
-    );
+    public final String food = "еда";
+    public final String other = "другое";
 
-    String buy = "{\"title\": \"булка\", \"date\": \"2022.02.08\", \"sum\": 200}";
-    public FinanceManager financeManager = new FinanceManager(categories);
+    public Object[] valuesBuy = new Object[]{food, "2022.08.02", 200L};
+    public FinanceManager financeManager = new FinanceManager();
+
+    @Test
+    public void getMaxOfMapTest() {
+        Map<String, Long> subMap = Map.of(food, 200L, other, 300L);
+        Long expected = 300L;
+        Long actual = financeManager.getMaxOfMap(subMap).get(other);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxCategoryOfPeriodTest() {
+        Map<String, Map<String, Long>> spendCategoryOfPeriod = Map.of(
+                food, Map.of("2022", 200L),
+                other, Map.of("2022", 300L)
+        );
+        String period = "2022";
+        Long expected = 300L;
+        Long actual = financeManager.maxCategoryOfPeriod(spendCategoryOfPeriod, period).get(other);
+
+        Assertions.assertEquals(expected,actual);
+    }
 
     @Test
     public void addBuyTest() {
         Long expectedSum = 200L;
 
-        financeManager.addBuy(buy);
-        Long actualSum = financeManager.getSpendEntirePeriod().get("еда");
+        financeManager.addBuy(valuesBuy);
+        Long actualSum = financeManager.getSpendEntirePeriod().get(food);
 
         Assertions.assertEquals(expectedSum, actualSum);
     }
 
     @Test
     public void maxCategoryTest() {
-        String expected = "{\"maxCategory\":[{\"sum\":200,\"category\":\"еда\"}]}";
+        int expectedSize = 4;
 
-        financeManager.addBuy(buy);
-        String actual = financeManager.maxCategory();
+        financeManager.addBuy(valuesBuy);
+        int actualSize = financeManager.maxCategory().size();
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expectedSize, actualSize);
     }
 }
